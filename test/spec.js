0a300885da4bda73423a31f0500c8b919bb5c32d
奇阳 <zongquan.hzq@alibaba-inc.com>
2015-12-23 15:59:41 +0800

    refactor(redux-sequence-action): change sequence-action structure and support parallel action

--- test/spec.js
+++ test/spec.js
@@ -11,7 +11,7 @@ function next(action) {
 }
 
 describe('sequenceAction', function(){
-  it('should pass if no `steps` set', function(){
+  it('should pass if action is not an array', function(){
     var action = {
       type: 'ACTION',
       payload: 1
@@ -22,76 +22,29 @@ describe('sequenceAction', function(){
     expect(spy).to.have.been.called.with(action);
   });
 
-  it('should pass if `steps` are not array', function(){
-    var stepAction = {
-      type: 'ACTION',
-      steps: {
-        a: 1
-      }
-    };
-    var spy = chai.spy(next);
-    sequenceAction({})(spy)(stepAction);
-
-    expect(spy).to.have.been.called.with(stepAction);
-  });
-
-  it('should pass if any of `steps` array is not a function', function(){
-    var stepAction = {
-      type: 'ACTION',
-      steps: [1, 2, function(){}]
-    };
-    var spy = chai.spy(next);
-    sequenceAction({})(spy)(stepAction);
-
-    expect(spy).to.have.been.called.with(stepAction);
-  });
-
-  it('should call functions defined in `steps`', function(){
-    var mockStep1 = function(dispatch, getState) {
-      expect(getState()).to.equal({a: 1});
-    };
-
-    var mockStep2 = function(dispatch, getState) {
-      expect(getState()).to.equal({a: 1});
-    };
-
-    var stepAction = {
-      steps: [ mockStep1, mockStep2 ]
-    };
-
-    var mockGetState = function() {
-      return {
-        a: 1
-      };
-    };
-
-    var store = {dispatch: {}, getState: mockGetState};
-
-    sequenceAction(store)(next)(stepAction);
-  });
-
-  it('should let second function get the latest state', function(){
-    var mockStore = {
+  it('should let second action get the latest state', function(){
+    var mockState = {
       a: 1
     };
-    var mockDispatch = (action) => {
-      mockStore.a = 2;
+
+    var mockDispatch = function(action) {
+      //mock thunkMiddleware
+      if(typeof action === 'function') {
+        return action();
+      }
+
+      mockState.a = action.payload;
+
       return action;
     };
 
-    var mockStep1 = function(dispatch, getState) {
-      dispatch({
-        type: 'SOME_TYPE'
-      });
+    var mockAction1 = { type: 'SOME_TYPE', payload: 2 };
+
+    var mockAction2 = function() {
+      expect(mockState).to.equal({a: 2});
     };
 
-    var mockStep2 = function() {
-      expect(mockStore).to.equal({a: 2});
-    };
-
-    var stepAction = {
-      steps: [ mockStep1, mockStep2 ]
-    };
+    var stepAction = [ mockAction1, mockAction2 ];
 
     sequenceAction({})(next)(stepAction);
   });
